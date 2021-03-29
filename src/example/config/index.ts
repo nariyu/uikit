6114bebcbae299e665e5d032a9f63a5fc99e3d13
nariyu <nariyu@playwell.jp>
2021-03-30 03:06:59 +0900

    Test #18

--- src/example/config/index.ts
+++ src/example/config/index.ts
@@ -4,5 +4,5 @@ export const Config = {
   NODE_ENV: process.env.NODE_ENV as 'development' | 'production',
   ENV: process.env.ENV as 'local' | 'develop' | 'staging' | 'production',
   VERCEL_URL: process.env.VERCEL_URL as string,
-  VERCEL_GIT_COMMIT_SHA: process.env.VERCEL_GIT_COMMIT_SHA as string,
+  VERCEL_GIT_COMMIT_SHA: (process.env.VERCEL_GIT_COMMIT_SHA || '').substr(0, 7),
 };
